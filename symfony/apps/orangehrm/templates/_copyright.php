d32cd4872f80282cacd88e98ef9cf32641c7290c
Vivian Brown <vivian@hr.(none)>
2015-08-13 12:17:51 -0700

    Initial commit.

---
+++ symfony/apps/orangehrm/templates/_copyright.php
@@ -0,0 +1,8 @@
+<?php 
+$version = '3.3';
+$prodName = 'OrangeHRM';
+$copyrightYear = date('Y');
+
+?>
+<?php echo $prodName . ' ' . $version;?><br/>
+
---
+++ symfony/web/resource_dir_inc.php
@@ -0,0 +1,5 @@
+<?php 
+sfConfig::set('sf_web_css_dir_name','webres_5523b60dca2e19.91394908/css');
+sfConfig::set('sf_web_js_dir_name','webres_5523b60dca2e19.91394908/js');
+sfConfig::set('sf_web_images_dir_name','webres_5523b60dca2e19.91394908/images');
+sfConfig::set('ohrm_resource_dir', 'webres_5523b60dca2e19.91394908');
